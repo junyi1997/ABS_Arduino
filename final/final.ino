4b012d7c884d0f275b89e7c7b1a5da6e007848b2
JohnnieWu <q5896799@gmail.com>
2019-08-19 16:46:41 +0800

    'Add_Manual_brake'

--- final/final.ino
+++ final/final.ino
@@ -1,16 +1,21 @@
 #define INTERVAL_MESSAGE1 30000
-#define INTERVAL_MESSAGE2 500
+#define INTERVAL_MESSAGE2 300
 unsigned long time_1 = 0;
 unsigned long time_2 = 0;
 bool _ABVAR_1_BTS= false ;
 bool _ABVAR_2_BTL= false ;
 int _ABVAR_3_count = 0 ;
-int A[300];
+int A[100];
 int a=0;
-const int motorIn1=5;
-const int motorIn2=6;
+int i=0;
+const int motorIn1=5;//定義馬達接腳1為5
+const int motorIn2=6;//定義馬達接腳2為6
+int Btn_ABS_ONOFF=7; //定義ABS開啟按鈕為7
+int Btn_Stop=8;      //定義煞車按鈕為8
 void setup() {
   pinMode( 2 , INPUT);
+  pinMode( Btn_ABS_ONOFF , INPUT);
+  pinMode( Btn_Stop , INPUT);
   Serial.begin(9600);
   pinMode(motorIn1,OUTPUT);
   pinMode(motorIn2,OUTPUT);
@@ -18,18 +23,22 @@ void setup() {
 }
  
 void loop() {
-  count_step();
-  
   unsigned long currentTime2 = millis();
   unsigned long currentTime1 = millis();
   if ((currentTime1-time_1)==INTERVAL_MESSAGE2){
     count_sort();
     time_1=currentTime1;
-  }
-  else if((currentTime2-time_2)>INTERVAL_MESSAGE1){
+  }else if((currentTime2-time_2)>INTERVAL_MESSAGE1){
     count_Total();
     time_2=currentTime2;
-  } 
+  }else{
+    count_step();
+    //Serial.println(digitalRead(Btn_ABS_ONOFF));
+    if(digitalRead(Btn_Stop)==1){
+         step_stop();
+         delay(300);
+    }
+  }
 }
 void count_step(){
   _ABVAR_1_BTS = digitalRead(2) ;
@@ -45,17 +54,21 @@ void count_step(){
 
 void count_sort(){
   A[a]+=_ABVAR_3_count;
+  Serial.print(i);
+  Serial.print(" ");
   Serial.println(_ABVAR_3_count);
-  if(_ABVAR_3_count>20){
+  i++;
+  if(_ABVAR_3_count>15){
+    if(digitalRead(Btn_ABS_ONOFF)==1){
     step_stop();
-    } 
+    }} 
   _ABVAR_3_count=0;
   }
 
 void count_Total(){
   Serial.print("A[");
   Serial.print(a);
-  Serial.print("}");
+  Serial.print("]");
   Serial.print(A[a]);
   a++;
   Serial.println();
@@ -63,7 +76,7 @@ void count_Total(){
 
 void step_stop(){
   analogWrite(motorIn1,255);
-  delay(170);  
+  delay(90);  
   analogWrite(motorIn1,0);
   delay(3);
   }     
