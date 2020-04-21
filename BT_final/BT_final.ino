6b636cc0ef5850e554fc8cc4f04882b674ce9a74
JohnnieWu <q5896799@gmail.com>
2020-04-21 14:43:49 +0800

    '0421_BT_test'

--- BT_final/BT_final.ino
+++ BT_final/BT_final.ino
@@ -1,22 +1,37 @@
+//------Arduino藍芽設定------//
+#include <SoftwareSerial.h>
+SoftwareSerial BT(9,10);   // 接收腳(RX), 傳送腳(TX)；接HC-06之TXD、RXD；先不要用0,1,因為USB用
+char val; //接收字串
+
+//-----millis計數器-----//
 #define INTERVAL_MESSAGE1 30000
 #define INTERVAL_MESSAGE2 300
 unsigned long time_1 = 0;
 unsigned long time_2 = 0;
+
+//------轉速觸發------//
 bool _ABVAR_1_BTS= false ;
 bool _ABVAR_2_BTL= false ;
 int _ABVAR_3_count = 0 ;
-int A[100];
-int a=0;
-int i=0;
+
+//------暫存器------//
+int A[100];//紀錄轉速陣列
+int a=0;//陣列計數器
+int i=0;//抬頭數字
+
+//------Arduino腳位設定------//
 const int motorIn1=5;//定義馬達接腳1為5
 const int motorIn2=6;//定義馬達接腳2為6
 int Btn_ABS_ONOFF=7; //定義ABS開啟按鈕為7
 int Btn_Stop=8;      //定義煞車按鈕為8
+
+
 void setup() {
   pinMode( 2 , INPUT);
   pinMode( Btn_ABS_ONOFF , INPUT);
   pinMode( Btn_Stop , INPUT);
   Serial.begin(9600);
+  BT.begin(9600);
   pinMode(motorIn1,OUTPUT);
   pinMode(motorIn2,OUTPUT);
   analogWrite(motorIn2,0);
@@ -37,6 +52,10 @@ void loop() {
     //Serial.println(digitalRead(Btn_ABS_ONOFF));
     
   }
+  /*if(BT.available()){
+    val=BT.read();
+    Serial.println(val);
+  }*/
 }
 void count_step(){
   _ABVAR_1_BTS = digitalRead(2) ;
@@ -48,13 +67,18 @@ void count_step(){
     }
   }
   _ABVAR_2_BTL = _ABVAR_1_BTS ;
+  
   }
 
 void count_sort(){
   A[a]+=_ABVAR_3_count;
   Serial.print(i);
   Serial.print(" ");
+  BT.print(_ABVAR_3_count);
+  //BT.println("12");
+ 
   Serial.println(_ABVAR_3_count);
+  
   i++;
   if(_ABVAR_3_count>=15){
     if(digitalRead(Btn_ABS_ONOFF)==1){
